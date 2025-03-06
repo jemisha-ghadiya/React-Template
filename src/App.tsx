ce65d968d915039ffa5a1f10662a10a7b03013e0
jemisha <ghadiyajemisha04@gmail.com>
2025-03-06 15:41:35 +0530

    husky and lint-staged

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+// import { Counter } from './Counter'
+import "./style.css";
+import IMAGE from "./react.png";
+import LOGO from './react-2.svg'
+import { Counter } from "./Counter";
+export const App = () => {
+  const name="jemisha"
+  return (
+    <>
+    
+      <h1>hello React TypeScript Webpack Starter Template-{process.env.NODE_ENV} {process.env.name}</h1>
+      <img src={IMAGE} alt="REACT LOGO"></img>
+      <img src={LOGO} alt="REACT LOGO" height="200px" width="100px"></img>
+      <Counter/>
+    </>
+  );
+};
---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom'
+import { App } from './App'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+// ReactDOM.render(<App />, document.getElementById('root'))
+// import React from 'react'
+createRoot(document.getElementById('root')).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+  
